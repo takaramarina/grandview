8e29d5b1707ee05ea961de8d1e758f63960f19c1
takaramarina <takaramarina@team-lab.com>
2025-09-30 12:36:45 +0900

    new manifesto

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,28 @@ export default function AboutPage() {
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold mt-6 sm:mt-8 mb-12 text-center">ABOUT</h1>
       <div className="max-w-3xl mx-auto">
         <p className="text-gray-700 leading-relaxed text-lg">
-            In a world with quick, subjective experiences, there is an objective strength in a language that is built from layers of intention and movement. One obvious example may be the visual arts, but expression is not limited to a specific medium; anything could be a linguistic vehicle, even the way that a room gets cleaned with care. When you continuously develop your own way of expression through a highly curated practice, you wield your own language with the most amount of honesty, freedom, and trust. Every experience is an opportunity to develop a purer expression.
-            <br /> <br /> But where does it end? When is it complete? It never is. Layers can always be added, there can always be more, and that might sound like a never-ending nightmare, but is life all about the convenience? What is a body for if not for the experience? What is our time for if not to live it to the fullest?
+        Art is not separate from life.
+        <br />It is in the everyday — in care, in attention.
+        <br />To create is to live, and every human is already an artist.
+
+        <br /><br />We live in an age of speed and distraction. Algorithms polarize us, convenience numbs us.
+        <br />But if we slow down, if we listen deeply, we recognize that every gesture can be layered with meaning. Through practice, we shape our own language with the most amount of honesty, freedom, and trust.
+
+        <br /><br />Within this strong language, listening is a form of speech.
+        <br />Imagine a world without miscommunication,
+        <br />where technology amplifies understanding,
+        <br />where truth is spoken and heard.
+        <br />If everyone spoke truth and listened, all fights would end.
+
+        <br /><br />Expression is never complete — and that is its power.
+        <br />Life is not for convenience; it is for experience.
+        <br />Art is not just a tool for decoration or entertainment; it is distinctly human, a language born from movement, intention, and care.
+
+        <br /><br />This is where we stand:
+        <br />A world where art belongs to everyone.
+        <br />A world where listening is creation.
+        <br />A world without miscommunication.
+        <br />A world lived to the fullest.
         </p>
       </div>
     </main>
